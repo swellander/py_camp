dd3dc59405c46427fcb29e58e55fb84afaf12e91
Sam Wellander <samwellander@gmail.com>
2019-02-27 11:26:42 -0500

    007

--- methods_functions/function_practice.py
+++ methods_functions/function_practice.py
@@ -8,9 +8,9 @@ def has_33(nums):
         return True
   return False
 
-print(has_33([1, 3, 3])) # → True
-print(has_33([1, 3, 1, 3]))# → False
-print(has_33([3, 1, 3]))# → False
+# print(has_33([1, 3, 3])) # → True
+# print(has_33([1, 3, 1, 3]))# → False
+# print(has_33([3, 1, 3]))# → False
 
 
 
@@ -22,3 +22,43 @@ def paper_doll(text):
   return ''.join([letter*3 for letter in text])
 
 print(paper_doll('Hello'))
+
+
+
+# BLACKJACK: Given three integers between 1 and 11, if their sum is less than or equal to 21, return their sum. If their sum exceeds 21 and there's an eleven, reduce the total sum by 10. Finally, if the sum (even after adjustment) exceeds 21, return 'BUST'
+# blackjack(5,6,7) --> 18
+# blackjack(9,9,9) --> 'BUST'
+# blackjack(9,9,11) --> 19
+
+def blackjack(a, b, c): 
+  cards = [a, b, c]
+  total = sum(cards)
+
+  if total > 21:
+    if 11 in cards:
+      total -= 10
+    if total > 21: 
+      return 'Bust'
+    else:
+      return total
+  else:
+    return total
+
+
+# print(blackjack(5,6,7))
+# print(blackjack(9,9,9))
+# print(blackjack(9,9,11))
+
+
+def spy_game(nums):
+  target = [0, 0, 7]
+  idx = 0 
+  for num in nums:
+    if idx > 2: break
+    if (num == target[idx]): idx += 1
+  if idx >= 3: return True
+  else: return False
+
+print(spy_game([1,2,4,0,0,7,5]))
+print(spy_game([1,0,2,4,0,5,7]))
+print(spy_game([1,7,2,0,4,5,0]))
